29f80ab60d11e3cfb06c0a9a0676bf0979957e2a
xuhaoqing <xuhaoqing@xinye.com>
2020-12-23 21:56:00 +0800

    区分类型

---
+++ src/leetcode_二叉树/no103_test.go
@@ -0,0 +1,113 @@
+package binary_tree_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+
+	var res [][]int
+
+	if root == nil {
+		return res
+	}
+
+	treeNodeList := []*TreeNode{root}
+
+	for i := 0; len(treeNodeList) > 0; i++ {
+		lenTreeNodeList := len(treeNodeList)
+		ans := make([]int, 0)
+
+		for _, treeNode := range treeNodeList {
+			ans = append(ans, treeNode.Val)
+
+			if treeNode.Left != nil {
+				treeNodeList = append(treeNodeList, treeNode.Left)
+			}
+
+			if treeNode.Right != nil {
+				treeNodeList = append(treeNodeList, treeNode.Right)
+			}
+		}
+		if i%2 == 1 {
+			for i, n := 0, len(ans); i < n/2; i++ {
+				ans[i], ans[len(ans)-i-1] = ans[len(ans)-i-1], ans[i]
+			}
+		}
+		res = append(res, ans)
+		treeNodeList = treeNodeList[lenTreeNodeList:]
+	}
+	return res
+
+	// var res [][]int
+
+	// if root == nil {
+	// 	return res
+	// }
+
+	// queue := make([]*TreeNode, 0)
+	// queue = append(queue, root)
+	// isLeftStart := true
+
+	// for len(queue) > 0 {
+	// 	queueLength := len(queue)
+	// 	fmt.Println(queueLength)
+	// 	ans := make([]int, queueLength)
+
+	// 	for i := 0; i < queueLength; i++ {
+	// 		node := queue[i]
+	// 		if node.Left != nil {
+	// 			queue = append(queue, node.Left)
+	// 		}
+	// 		if node.Right != nil {
+	// 			queue = append(queue, node.Right)
+	// 		}
+	// 		if isLeftStart {
+	// 			ans[i] = node.Val
+	// 		} else {
+	// 			ans[queueLength-i-1] = node.Val
+	// 		}
+	// 	}
+	// 	res = append(res, ans)
+	// 	isLeftStart = !isLeftStart
+	// 	queue = queue[queueLength:]
+	// }
+	// return res
+}
+func TestFunc103(t *testing.T) {
+
+	var root *TreeNode
+	// [3,9,20,null,null,15,7],
+	root = &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val:   9,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val:   15,
+				Left:  nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val:   7,
+				Left:  nil,
+				Right: nil,
+			},
+		},
+	}
+
+	res := zigzagLevelOrder(root)
+
+	fmt.Println(res)
+}
---
+++ src/leetcode_二叉树/no124_test.go
@@ -0,0 +1,10 @@
+package binary_tree_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFunc124(t *testing.T) {
+	fmt.Println(1)
+}
