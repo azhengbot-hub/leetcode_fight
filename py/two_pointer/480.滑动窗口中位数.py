c375d0cca3886295200c68a7ec8cfc431201b7ad
xuhaoqing <xuhaoqing@xinye.com>
2021-02-04 16:41:57 +0800

    no480

--- py/two_pointer/480.滑动窗口中位数.py
+++ py/two_pointer/480.滑动窗口中位数.py
@@ -7,5 +7,26 @@
 # @lc code=start
 class Solution:
     def medianSlidingWindow(self, nums: list, k: int) -> list:
-        pass
+
+        left = 0
+        res_list = []
+
+        while left+k <= len(nums):
+            aim_nums = sorted(nums[left:left+k])
+
+            if k % 2 == 0:
+                res = (aim_nums[int(k/2)] + aim_nums[int(k/2)-1])/2
+            else:
+                res = aim_nums[int((k-1)/2)]
+
+            res_list.append(res)
+
+            left += 1
+
+        return res_list
 # @lc code=end
+
+
+s = Solution()
+
+print(s.medianSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
